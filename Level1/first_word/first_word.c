25d2624acd435aa36c0e377880c74138d3373f89
Beyzanur Bektas <bektas.beyza@hotmail.com>
2023-10-08 22:33:13 +0300

    success

--- Level1/first_word/first_word.c
+++ Level1/first_word/first_word.c
@@ -4,9 +4,9 @@ int main(int ac, char **av)
     if(ac == 2)
     {
         int i = 0;
-        while(av[1][i] <= 32 && av[1][i])
+        while(av[1][i] && av[1][i] <= 32)
             i++;
-        while(av[1][i] > 32 && av[1][i])
+        while(av[1][i] && av[1][i] > 32)
         {
             write(1, &av[1][i], 1);
             i++;
