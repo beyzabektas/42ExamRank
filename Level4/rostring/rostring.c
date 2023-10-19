5a2f31395a4620bde8804be25868390710d36e6e
beyzabektas <bektas.beyza@hotmail.com>
2023-10-20 00:03:01 +0300

    success

--- Level4/rostring/rostring.c
+++ Level4/rostring/rostring.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
     int i;
     int start;
@@ -8,31 +8,31 @@ int main(int argc, char **argv)
     int flag;
 
     flag = 0;
-    if (argc > 1 && argv[1][0])
+    if (ac > 1 && av[1][0])
     {
         i = 0;
-        while (argv[1][i] <= 32 && argv[1][i]) 
+        while (av[1][i] <= 32 && av[1][i]) 
             i++;
         start = i;
-        while (argv[1][i] && argv[1][i] > 32)
+        while (av[1][i] && av[1][i] > 32)
             i++;
         end = i;
-        while (argv[1][i] && argv[1][i] <= 32)
+        while (av[1][i] && av[1][i] <= 32)
             i++;
-        while(argv[1][i]) 
+        while(av[1][i]) 
         {
-            while ((argv[1][i] && argv[1][i] <= 32 && argv[1][i + 1] <= 32))
+            while ((av[1][i] && av[1][i] <= 32 && av[1][i + 1] <= 32))
                 i++; 
-            if (argv[1][i] <= 32)
+            if (av[1][i] <= 32)
                 flag = 1; 
-            write(1, &argv[1][i], 1);
+            write(1, &av[1][i], 1);
             i++;
         }
         if (flag)
             write(1, " ", 1);
         while (start < end)
         {
-            write(1, &argv[1][start], 1);
+            write(1, &av[1][start], 1);
             start++;
         }
     }
